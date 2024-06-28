1ace649b26f89b3ce06262a84c22c569244c30bb
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-06-28 15:52:22 +0200

    chore: delete unnecessary lines

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,23 +1,21 @@
-import dotenv from 'dotenv';
 import express from 'express';
-import { connectToDatabase } from './driver/driver'; 
+import { connectToDatabase } from './driver/driver';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-async function startServer(dbName:string) {
-  try {
-    dotenv.config()
-    await connectToDatabase(dbName); 
-    console.log('Connected to database successfully');
+async function startServer(dbName: string) {
+	try {
+		await connectToDatabase(dbName);
+		console.log('Connected to database successfully');
 
-    app.listen(PORT, () => {
-      console.log(`Server is running on http://localhost:${PORT}`);
-    });
-  } catch (error) {
-    console.error('Failed to connect to the database', error);
-    process.exit(1); 
-  }
+		app.listen(PORT, () => {
+			console.log(`Server is running on http://localhost:${PORT}`);
+		});
+	} catch (error) {
+		console.error('Failed to connect to the database', error);
+		process.exit(1);
+	}
 }
 
 startServer('sake');
