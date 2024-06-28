06193e7a442b74812913b49825f3551ce7c1f43a
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-06-28 16:46:23 +0200

    feat: insert breweries

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,8 @@ import { DataLoaderFromAPIToMongoDB } from './services/data_loader';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const THIRD_PARTY_API = 'https://muro.sakenowa.com/sakenowa-data/api'
+
 async function startServer(dbName: string) {
 	try {
 		const db = await connectToDatabase(dbName);
@@ -13,11 +15,11 @@ async function startServer(dbName: string) {
 		// Load data
 		const loader = new DataLoaderFromAPIToMongoDB(
 			db,
-			'areas',
-			'https://muro.sakenowa.com/sakenowa-data/api/areas'
+			'breweries',
+			THIRD_PARTY_API +'/breweries'
 		);
 		// console.log(await loader.fetch_url())
-		// await loader.insert_data('areas');
+		// await loader.insert_data('breweries');
 
 		app.listen(PORT, () => {
 			console.log(`Server is running on http://localhost:${PORT}`);
