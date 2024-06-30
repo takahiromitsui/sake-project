2fcb3bfebfb1cd545a9eb9a5693702838ddf76ed
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-06-30 17:31:27 +0200

    feat: create winston logger

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
-import express, { NextFunction, Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
+import winston from 'winston';
 
 import { connectionString } from './db/connection';
 import { getBrandsByAreaName } from './controllers/brands';
@@ -8,25 +9,31 @@ import ApiError from './helpers/api_error';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get(
-	'/brands/:areaName',
-	async (req: Request, res: Response, next: NextFunction) => {
-		try {
-			const { areaName } = req.params;
-			const brands = await getBrandsByAreaName(areaName);
-			if (brands instanceof ApiError) {
-				return res.status(brands.status).json({ message: brands.message });
-			}
-			res.status(200).json({
-				brands: brands,
-			});
-		} catch (error) {
-			res.status(500).json({
-				message: 'Internal server error',
-			});
+const logger = winston.createLogger({
+	level: 'info',
+	format: winston.format.cli(),
+	transports: [new winston.transports.Console()],
+});
+
+app.get('/brands/:areaName', async (req: Request, res: Response) => {
+	try {
+		const { areaName } = req.params;
+		const brands = await getBrandsByAreaName(areaName);
+		if (brands instanceof ApiError) {
+			logger.error(`${brands.status}: ${brands.message}`);
+			return res.status(brands.status).json({ message: brands.message });
 		}
+		logger.info('200: Success');
+		res.status(200).json({
+			brands: brands,
+		});
+	} catch (error) {
+		logger.info(`500: ${error}`);
+		res.status(500).json({
+			message: 'Internal server error',
+		});
 	}
-);
+});
 
 async function startServer() {
 	try {
