1a93c727f87c7e50d84fa02b3317b0e30d12d17d
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-06-30 16:16:20 +0200

    feat: create a route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,57 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
+import { Area } from './models/area';
 
 import { connectionString } from './db/connection';
+import { Brewery } from './models/brewery';
+import { Brand } from './models/brand';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.get('/brands/:areaName', async (req: Request, res: Response) => {
+	try {
+		const { areaName } = req.params;
+		const area = await Area.findOne({
+			en: areaName,
+		});
+		if (!area) {
+			//add logger
+			return res.status(404).json({
+				message: 'area not found',
+			});
+		} else {
+			const breweries = await Brewery.find({ area: area });
+
+			if (!breweries || breweries.length === 0) {
+				// Add logger
+				return res.status(404).json({
+					message: 'Breweries not found for the given area',
+				});
+			}
+			const brands = await Brand.find({ brewery: { $in: breweries } })
+				.populate({
+					path: 'brewery',
+					populate: {
+						path: 'area',
+						model: 'Area'
+					}
+				})
+				.exec();
+
+			// Add logger
+			res.status(200).json({
+				brands: brands,
+			});
+		}
+	} catch (error) {
+		// add logger
+		console.log(error);
+		res.status(500).json({
+			message: 'server error',
+		});
+	}
+});
 
 async function startServer() {
 	try {
