c35b43541136cb0fd1b9486ecb474cf04a87e890
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-07-03 14:26:06 +0200

    feat: add leaflet map

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,28 @@
-import Image from 'next/image';
+'use client';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
 
 export default function Home() {
 	return (
-		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      Initial settings
-    </main>
+		<MapContainer
+			style={{
+				height: '100vh',
+				zIndex: 0,
+			}}
+			center={[36.00, 138.00]}
+			zoom={4}
+			scrollWheelZoom={false}
+		>
+			<TileLayer
+				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+				url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+			/>
+			{/* <Marker position={[51.505, -0.09]}>
+				<Popup>
+					A pretty CSS3 popup. <br /> Easily customizable.
+				</Popup>
+			</Marker> */}
+		</MapContainer>
 	);
 }
