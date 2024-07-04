d3b2f44fedfbcbf3ced64d687c4075f6b175a66f
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-07-04 15:47:12 +0200

    feat: allow COR

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 
 import { connectionString } from './db/connection';
 import { router as BrandRoutes } from './routes/brands';
 import { logger } from './helpers/logger';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
+app.use(cors())
 app.use('/brands', BrandRoutes);
 
 async function startServer() {
