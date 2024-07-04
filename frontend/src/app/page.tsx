d784cee3d7817a37d72089def41a01aee1cfe4bc
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-07-04 17:22:59 +0200

    feat: add marker

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,37 @@
 'use client';
+import { Marker, Popup } from 'react-leaflet';
+import { Icon } from 'leaflet';
 import LeafletMap from '@/components/leaflet-map';
 
 export default function Home() {
+	const markers: [number, number][] = [
+		[35.6895, 139.6917], // Tokyo
+		[34.6937, 135.5023], // Osaka
+		[35.1815, 136.9066], // Nagoya
+		[43.0686, 141.3508], // Sapporo
+		[35.6894, 139.6917], // Tokyo (again)
+	];
+	const popups: string[] = [
+		'Tokyo',
+		'Osaka',
+		'Nagoya',
+		'Sapporo',
+		'Tokyo (again)',
+	];
+	const iconURL = '/assets/marker-icon.png';
+
+	const customIcon = new Icon({
+		iconUrl: iconURL,
+		iconSize: [38, 38],
+	});
+	// const createCustomClusterIcon = (cluster) => {
+	// 	const count = cluster.getChildCount();
+	// 	return new divIcon({
+	// 		html: `<div class='cluster-icon>${count}</div>`,
+	// 		className: 'cluster-marker-cluster',
+	// 		iconSize: point(33,33, true),
+	// 	});
+	// }
 	return (
 		<LeafletMap
 			style={{
@@ -17,7 +47,11 @@ export default function Home() {
 			minZoom={5}
 			scrollWheelZoom={false}
 		>
-			<div></div>
+			{markers.map((position, index) => (
+				<Marker key={index} position={position} icon={customIcon}>
+					<Popup>{popups[index]}</Popup>
+				</Marker>
+			))}
 		</LeafletMap>
 	);
 }
