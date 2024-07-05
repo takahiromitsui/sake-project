99a2960555ca372dda78cb24843fa0d527413d17
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-07-05 09:55:44 +0200

    feat: add location selector

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,16 @@
 'use client';
-
+import {
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectLabel,
+	SelectTrigger,
+	SelectValue,
+} from '@/components/ui/select';
 import LeafletMap from '@/components/leaflet-map';
 import dynamic from 'next/dynamic';
+import { prefectures } from '@/lib/prefectures';
 
 export default function Home() {
 	const LazyMarkers = dynamic(
@@ -11,22 +20,41 @@ export default function Home() {
 		}
 	);
 	return (
-		<LeafletMap
-			style={{
-				height: '100vh',
-				width: '100vw',
-			}}
-			bounds={[
-				[20.358, 122.934], // Southwest corner (approx)
-				[45.551, 153.986], // Northeast corner (approx)
-			]}
-			center={[36.2048, 138.2529]}
-			zoom={5}
-			minZoom={5}
-			maxZoom={10}
-			scrollWheelZoom={false}
-		>
-			<LazyMarkers />
-		</LeafletMap>
+		<div className='flex'>
+			<div className='w-1/4  pt-4 pl-4 pr-4'>
+				<Select>
+					<SelectTrigger className='w-[180px]'>
+						<SelectValue placeholder='Select a location' />
+					</SelectTrigger>
+					<SelectContent>
+						<SelectGroup>
+							<SelectLabel>Location</SelectLabel>
+							{prefectures.map(prefecture => (
+								<SelectItem key={prefecture.name} value={prefecture.name}>
+									{prefecture.display}
+								</SelectItem>
+							))}
+						</SelectGroup>
+					</SelectContent>
+				</Select>
+			</div>
+			<LeafletMap
+				style={{
+					height: '100vh',
+					width: '100vw',
+				}}
+				bounds={[
+					[20.358, 122.934], // Southwest corner (approx)
+					[45.551, 153.986], // Northeast corner (approx)
+				]}
+				center={[36.2048, 138.2529]}
+				zoom={5}
+				minZoom={5}
+				maxZoom={10}
+				scrollWheelZoom={false}
+			>
+				<LazyMarkers />
+			</LeafletMap>
+		</div>
 	);
 }
---
+++ frontend/src/lib/prefectures.ts
@@ -0,0 +1,242 @@
+export const prefectures = [
+	{
+		name: 'hokkaido',
+		display: '1. Hokkaido',
+		coordinate: [43.06417, 141.34694],
+	},
+	{
+		name: 'aomori',
+		display: '2. Aomori',
+		coordinate: [40.82444, 140.74],
+	},
+	{
+		name: 'iwate',
+		display: '3. Iwate',
+		coordinate: [39.70361, 141.1525],
+	},
+	{
+		name: 'miyagi',
+		display: '4. Miyagi',
+		coordinate: [38.26889, 140.87194],
+	},
+	{
+		name: 'akita',
+		display: '5. Akita',
+		coordinate: [39.71861, 140.1025],
+	},
+	{
+		name: 'yamagata',
+		display: '6. Yamagata',
+		coordinate: [38.24056, 140.36333],
+	},
+	{
+		name: 'fukushima',
+		display: '7. Fukushima',
+		coordinate: [37.75, 140.46778],
+	},
+	{
+		name: 'ibaraki',
+		display: '8. Ibaraki',
+		coordinate: [36.34139, 140.44667],
+	},
+	{
+		name: 'tochigi',
+		display: '9. Tochigi',
+		coordinate: [36.56583, 139.88361],
+	},
+	{
+		name: 'gunma',
+		display: '10. Gunma',
+		coordinate: [36.39111, 139.06083],
+	},
+	{
+		name: 'saitama',
+		display: '11. Saitama',
+		coordinate: [35.85694, 139.64889],
+	},
+	{
+		name: 'chiba',
+		display: '12. Chiba',
+		coordinate: [35.60472, 140.12333],
+	},
+	{
+		name: 'tokyo',
+		display: '13. Tokyo',
+		coordinate: [35.68944, 139.69167],
+	},
+	{
+		name: 'kanagawa',
+		display: '14. Kanagawa',
+		coordinate: [35.44778, 139.6425],
+	},
+	{
+		name: 'niigata',
+		display: '15. Niigata',
+		coordinate: [37.90222, 139.02361],
+	},
+	{
+		name: 'toyama',
+		display: '16. Toyama',
+		coordinate: [36.69528, 137.21139],
+	},
+	{
+		name: 'ishikawa',
+		display: '17. Ishikawa',
+		coordinate: [36.59444, 136.62556],
+	},
+	{
+		name: 'fukui',
+		display: '18. Fukui',
+		coordinate: [36.06528, 136.22194],
+	},
+	{
+		name: 'yamanashi',
+		display: '19. Yamanashi',
+		coordinate: [35.66389, 138.56833],
+	},
+	{
+		name: 'nagano',
+		display: '20. Nagano',
+		coordinate: [36.65139, 138.18111],
+	},
+	{
+		name: 'gifu',
+		display: '21. Gifu',
+		coordinate: [35.39111, 136.72222],
+	},
+	{
+		name: 'shizuoka',
+		display: '22. Shizuoka',
+		coordinate: [34.97556, 138.38278],
+	},
+	{
+		name: 'aichi',
+		display: '23. Aichi',
+		coordinate: [35.18028, 136.90667],
+	},
+	{
+		name: 'mie',
+		display: '24. Mie',
+		coordinate: [34.73028, 136.50861],
+	},
+	{
+		name: 'shiga',
+		display: '25. Shiga',
+		coordinate: [35.00444, 135.86833],
+	},
+	{
+		name: 'kyoto',
+		display: '26. Kyoto',
+		coordinate: [35.02139, 135.75556],
+	},
+	{
+		name: 'osaka',
+		display: '27. Osaka',
+		coordinate: [34.68639, 135.52],
+	},
+	{
+		name: 'hyogo',
+		display: '28. Hyogo',
+		coordinate: [34.69139, 135.18306],
+	},
+	{
+		name: 'nara',
+		display: '29. Nara',
+		coordinate: [34.68528, 135.83278],
+	},
+	{
+		name: 'wakayama',
+		display: '30. Wakayama',
+		coordinate: [34.22611, 135.1675],
+	},
+	{
+		name: 'tottori',
+		display: '31. Tottori',
+		coordinate: [35.50361, 134.23833],
+	},
+	{
+		name: 'shimane',
+		display: '32. Shimane',
+		coordinate: [35.47222, 133.05056],
+	},
+	{
+		name: 'okayama',
+		display: '33. Okayama',
+		coordinate: [34.66167, 133.935],
+	},
+	{
+		name: 'hiroshima',
+		display: '34. Hiroshima',
+		coordinate: [34.39639, 132.45944],
+	},
+	{
+		name: 'yamaguchi',
+		display: '35. Yamaguchi',
+		coordinate: [34.18583, 131.47139],
+	},
+	{
+		name: 'tokushima',
+		display: '36. Tokushima',
+		coordinate: [34.06583, 134.55944],
+	},
+	{
+		name: 'kagawa',
+		display: '37. Kagawa',
+		coordinate: [34.34028, 134.04333],
+	},
+	{
+		name: 'ehime',
+		display: '38. Ehime',
+		coordinate: [33.84167, 132.76556],
+	},
+	{
+		name: 'kochi',
+		display: '39. Kochi',
+		coordinate: [33.55972, 133.53111],
+	},
+	{
+		name: 'fukuoka',
+		display: '40. Fukuoka',
+		coordinate: [33.60639, 130.41806],
+	},
+	{
+		name: 'saga',
+		display: '41. Saga',
+		coordinate: [33.24944, 130.29889],
+	},
+	{
+		name: 'nagasaki',
+		display: '42. Nagasaki',
+		coordinate: [32.74472, 129.87361],
+	},
+	{
+		name: 'kumamoto',
+		display: '43. Kumamoto',
+		coordinate: [32.78972, 130.74167],
+	},
+	{
+		name: 'oita',
+		display: '44. Oita',
+		coordinate: [33.23806, 131.6125],
+	},
+	{
+		name: 'miyazaki',
+		display: '45. Miyazaki',
+		coordinate: [31.90778, 131.42028],
+	},
+	{
+		name: 'kagoshima',
+		display: '46. Kagoshima',
+		coordinate: [31.56028, 130.55806],
+	},
+	{
+		name: 'okinawa',
+		display: '47. Okinawa',
+		coordinate: [26.2125, 127.68111],
+	},
+	{
+		name: 'others',
+		display: '48. Others',
+		coordinate: [],
+	},
+] as const;
