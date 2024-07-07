2287d7557c49d24696effc083aaf78aab72c19c4
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-07-07 16:12:11 +0200

    feat: define models

---
+++ backend-go/internal/models/models.go
@@ -0,0 +1,31 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// Assuming Area struct definition
+type Area struct {
+	MongoID primitive.ObjectID `bson:"_id,omitempty" json:"mongoId,omitempty"`
+	ID   int 								`bson:"id" json:"id"`
+	Name string             `bson:"name"`
+	En   string             `bson:"en"`
+}
+
+// Brewery struct definition
+type Brewery struct {
+	MongoID primitive.ObjectID `bson:"_id,omitempty" json:"mongoId,omitempty"`
+	ID    int                `bson:"id" json:"id"`
+	Name  string             `bson:"name" json:"name"`
+	En    string             `bson:"en" json:"en"`
+	Area  primitive.ObjectID `bson:"area" json:"area"`
+}
+
+// Brand struct definition
+type Brand struct {
+	MongoID primitive.ObjectID `bson:"_id,omitempty" json:"mongoId,omitempty"`
+	ID      int                `bson:"id" json:"id"`
+	Name    string             `bson:"name" json:"name"`
+	En      string             `bson:"en" json:"en"`
+	Brewery primitive.ObjectID `bson:"brewery" json:"brewery"`
+}
