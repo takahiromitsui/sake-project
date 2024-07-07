163b728b709158c16f59b473caa887f9cddcfe41
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-07-07 17:40:14 +0200

    fix: fix file errors

--- backend-go/internal/helpers/helpers.go
+++ backend-go/internal/helpers/helpers.go
@@ -12,37 +12,41 @@ import (
 )
 
 
-func ExportJson(client *mongo.Client, collection_name string) (error){
+func ExportJson(client *mongo.Client, collectionName string) (error){
+	fmt.Println("Exporting", collectionName)
 	const pageSize = 100
-	collection := client.Database("sake").Collection(collection_name)
-	total, err := collection.CountDocuments(context.TODO(), nil)
-	if err != nil {
-		return err
-	}
-	 totalPages := int(total / pageSize)
+	collection := client.Database("sake").Collection(collectionName)
+	total := 3167
+	totalPages := int(total / pageSize)
+	fmt.Println("Total Pages", totalPages)
 	for page := 0; page <= totalPages; page++ {
 		opts := options.Find().SetSkip(int64(page * pageSize)).SetLimit(pageSize)
 		cursor, err := collection.Find(context.TODO(), bson.D{}, opts)
 		if err != nil {
+			fmt.Println("Error finding documents")
 			return err
 		}
 		defer cursor.Close(context.TODO())
 		var results []bson.M
 		if err = cursor.All(context.TODO(), &results); err != nil {
+			fmt.Println("Error decoding documents")
 			return err
 		}
-		fileName := "internal/data/"+collection_name + "_" + string(page) + ".json"
+		fileName := fmt.Sprintf("%s_%d.json", collectionName, page)
 		file, err := os.Create(fileName)
 		if err != nil {
+			fmt.Println("Error creating file")
 			return err
 		}
 		defer file.Close()
 		jsonData, err := json.MarshalIndent(results, "", "  ")
 		if err != nil {
+			fmt.Println("Error marshalling data")
 			return err
 		}
 		_, err = file.Write(jsonData)
 		if err != nil {
+			fmt.Println("Error writing data")
 			return err
 		}
 		fmt.Println("Exported", fileName)
