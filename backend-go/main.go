11fc8878888d009ba8fb4c4e03c97c3c6e62c5a6
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-07-07 14:37:09 +0200

    feat: connect to server

--- backend-go/main.go
+++ backend-go/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"time"
 
@@ -11,20 +11,41 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+const port = ":8000"
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-    log.Fatal("Error loading .env file")
-  }
-
-	uri := os.Getenv("DB_CONN_STR")
-	fmt.Println(uri)
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-defer cancel()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	client, err := run()
 	if err != nil {
 		log.Fatal(err)
 	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 	defer client.Disconnect(ctx)
+	log.Println("Server running on port", port)
+	srv := &http.Server{
+		Addr: port,
+	}
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() (*mongo.Client, error){
+	err := godotenv.Load()
+	if err != nil {
+    log.Fatal("Error loading .env file")
+		return nil, err
+  }
+
+	uri := os.Getenv("DB_CONN_STR")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatal("Error connecting to MongoDB")
+		return nil, err
+	}
+	log.Println("Connected to database")
+	return client, nil
 }
